17c2f6cc2d0e639aa322ebeb58ab9eaa24d4798c
TPS CésarD <TPS CsarD@beatriz-PC>
2015-07-30 11:33:23 -0500

    Actividad Formulario de entrada -30.VII.2015- -Terminando presentación de la app. -Y proceso en desarrollo del file.php

--- index.php
+++ index.php
@@ -5,31 +5,70 @@
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
-        <h2>FORMULARIO PARA LA ENTRADA</h2>
+    <body background="DP.jpg" style="color: #33ccff">
+
+        <h2><font size=42 color="green" face="pricedown">APP DISFRUTECA</font></h2>
         <TABLE BORDER=3>
-        <form action="procesarFormulario.php" method="post">
-        <tr><th COLSPAN=2>
-        <label for="inputNombre">Nombre : </label>
-        <input required type="text" id="inputNombre" name="inputNombre" placeholder="Ingrese su nombre aquí, por favor">
-        <br/>    
-        <input type="radio" name="sexo" id="hombre" value="Hombre">
-        <label for="hombre">Hombre : </label>
-        <br/>
-        <input type="radio" name="sexo" id="mujer" value="Mujer">
-        <label for="mujer">Mujer: </label>
-        <br/>
-        <label for="inputEdad">Edad : </label>
-        <input required type="number" id="inputEdad" name="inputEdad">
-        <br/>
-        Ingresa tus comentarios si tienes alguna duda:
-        <br/>
-        <textarea name="comentarios" rows="2" cols="30">...Tus comentarios aquí...</textarea>
-        <tr><td><button type="submit">Registrar</button> <input type="reset">
+            <form action="procesarFormulario.php" method="post">
+                <tr>
+                    <td>
+                        <label for="inputNombre">Nombre : </label>
+                    </td>
+                    <td>
+                        <input required type="text" id="inputNombre" name="inputNombre" placeholder="Ingrese su nombre">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Sexo: </label>
+                    </td>
+                    <td>
+                        <input type="radio" name="sexo" id="hombre" value="Hombre">
+                        <label for="hombre" color="green"> Hombre</label>
+                        <input type="radio" name="sexo" id="mujer" value="Mujer">
+                        <label for="mujer"> Mujer</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="inputEdad">Edad : </label>
+                    </td>
+                    <td>
+                        <input required type="number" id="inputEdad" name="inputEdad" placeholder="Ingresa tu edad">
+                    </td>
+                </tr>
+                <tr>
+                <tr>
+                    <td>
+                        <label for="inputVestuario">Vestuario : </label>
+                    </td>
+                    <td>
+                        <select name="vestuario" size="1"> 
+                            <option value="r">Camisa manga larga con pantalon de tela y zapatos de material</option> 
+                            <option value="g">Camiseta manga corta con pantalon de pana y zapatos deportivos</option> 
+                            <option value="b"></option> 
+                        </select> 
+                    </td>
+                </tr>
+                <td>
+                    Comentarios:
+                </td>
+                <td>
+                    <textarea name="comentarios" rows="2" cols="30" placeholder="Ingresa tus comentarios aquí"></textarea>
+                </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button type="submit">Registrar</button>
+                    </td>
+                    <td>
+                        <input type="reset">
+                    </td>
+                </tr>
         </TABLE>    
-        </form>
-        
-        
-       
-    </body>
+    </form>
+
+
+
+</body>
 </html>
--- procesarFormulario.php
+++ procesarFormulario.php
@@ -2,8 +2,13 @@
 
 $strNombre = filter_input(INPUT_POST, 'inputNombre');
 
+echo $strNombre . "Gracias por acudir a nuestra app";
 
-echo $strNombre;
+$numEdad = filter_input(INPUT_POST, 'inputEdad');
+$strSexo = filter_input(INPUT_POST, 'sexo');
+$strVestuario = filter_input(INPUT_POST, 'inputVestuario');
 
 
-
+if ($numEdad < 18 && $strSexo && $strVestuario){
+    echo 'Lo siento, pero tu no puedes asistir a ningúno de nuestros lugares de ocio. ¡Buenas noches!';
+}
